8232ad29cf99d1bb93fcad9658e89636c3d2e0a7
Daniel Mendler <mail@daniel-mendler.de>
2021-05-28 12:33:19 +0200

    Add vertico-next/previous-group (Fix #47)
    Bound to forward/backward-paragraph

--- vertico.el
+++ vertico.el
@@ -96,6 +96,8 @@
     (define-key map [remap previous-line] #'vertico-previous)
     (define-key map [remap next-line-or-history-element] #'vertico-next)
     (define-key map [remap previous-line-or-history-element] #'vertico-previous)
+    (define-key map [remap backward-paragraph] #'vertico-previous-group)
+    (define-key map [remap forward-paragraph] #'vertico-next-group)
     (define-key map [remap exit-minibuffer] #'vertico-exit)
     (define-key map [remap kill-ring-save] #'vertico-save)
     (define-key map [C-return] #'vertico-exit-input)
@@ -558,6 +560,32 @@
                  (eq (ignore-errors (read-char "Confirm")) 13)))
         (exit-minibuffer)
       (message "Match required"))))
+
+(defun vertico--goto-group (next)
+  "Move to next group if NEXT is non-nil, otherwise move to previous group."
+  (let* ((end (minibuffer-prompt-end))
+         (metadata (completion-metadata (buffer-substring end (max end (point)))
+                                        minibuffer-completion-table
+                                        minibuffer-completion-predicate))
+         (group-fun (or (completion-metadata-get metadata 'group-function) #'ignore))
+         (orig-index vertico--index))
+    (while (let ((last-index vertico--index))
+             (if next (vertico-next) (vertico-previous))
+             (if (or (= vertico--index orig-index) (= vertico--index last-index))
+                 (and (vertico--goto orig-index) nil)
+               (and (> vertico--index 0)
+                    (equal (funcall group-fun (nth (1- vertico--index) vertico--candidates) nil)
+                           (funcall group-fun (nth vertico--index vertico--candidates) nil))))))))
+
+(defun vertico-next-group ()
+  "Move to next group."
+  (interactive)
+  (vertico--goto-group 'next))
+
+(defun vertico-previous-group ()
+  "Move to previous group."
+  (interactive)
+  (vertico--goto-group nil))
 
 (defun vertico-exit-input ()
   "Exit minibuffer with input."
