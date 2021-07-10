d57f2d762f64655f3e65fba9e88c5831b6e54582
Daniel Mendler <mail@daniel-mendler.de>
2021-07-11 00:36:57 +0200

    vertico-flat: Add highlighting

--- extensions/vertico-flat.el
+++ extensions/vertico-flat.el
@@ -68,6 +68,7 @@
          (result))
     (while (and candidates (> width 0) (> count 0))
       (let ((cand (car candidates)))
+        (setq cand (car (funcall vertico--highlight (list cand))))
         (when (string-match-p "\n" cand)
           (setq cand (vertico--truncate-multiline cand width)))
         (setq cand (string-trim
