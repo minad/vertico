b99ce0cd6ff646a47c261df85a53843baf637b97
Daniel Mendler <mail@daniel-mendler.de>
2021-12-01 18:09:55 +0100

    Version 0.17
    This release includes the extensions as part of the ELPA package.

--- extensions/vertico-flat.el
+++ extensions/vertico-flat.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Daniel Mendler <mail@daniel-mendler.de>
 ;; Created: 2021
 ;; Version: 0.1
-;; Package-Requires: ((emacs "27.1") (vertico "0.16"))
+;; Package-Requires: ((emacs "27.1") (vertico "0.17"))
 ;; Homepage: https://github.com/minad/vertico
 
 ;; This file is part of GNU Emacs.
