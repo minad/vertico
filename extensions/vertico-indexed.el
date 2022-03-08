e82b7ff371a348d9e5a1ac7bf4b652f0a2d38198
Daniel Mendler <mail@daniel-mendler.de>
2022-03-08 11:13:54 +0100

    Version 0.21

--- extensions/vertico-indexed.el
+++ extensions/vertico-indexed.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Daniel Mendler <mail@daniel-mendler.de>
 ;; Created: 2021
 ;; Version: 0.1
-;; Package-Requires: ((emacs "27.1") (vertico "0.20"))
+;; Package-Requires: ((emacs "27.1") (vertico "0.21"))
 ;; Homepage: https://github.com/minad/vertico
 
 ;; This file is part of GNU Emacs.
