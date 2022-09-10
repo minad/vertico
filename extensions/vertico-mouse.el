64b57b04521db4616ac77fa151d3225082ce44ed
Daniel Mendler <mail@daniel-mendler.de>
2022-09-10 21:56:17 +0200

    Version 0.27

--- extensions/vertico-mouse.el
+++ extensions/vertico-mouse.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Daniel Mendler <mail@daniel-mendler.de>
 ;; Created: 2021
 ;; Version: 0.1
-;; Package-Requires: ((emacs "27.1") (vertico "0.26"))
+;; Package-Requires: ((emacs "27.1") (vertico "0.27"))
 ;; Homepage: https://github.com/minad/vertico
 
 ;; This file is part of GNU Emacs.
