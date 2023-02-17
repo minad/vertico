01f0fc07d1831f00ebc64550867a0fa6eedb1fb7
Daniel Mendler <mail@daniel-mendler.de>
2023-02-17 22:02:42 +0100

    Version 1.1

--- vertico.el
+++ vertico.el
@@ -5,7 +5,7 @@
 ;; Author: Daniel Mendler <mail@daniel-mendler.de>
 ;; Maintainer: Daniel Mendler <mail@daniel-mendler.de>
 ;; Created: 2021
-;; Version: 1.0
+;; Version: 1.1
 ;; Package-Requires: ((emacs "27.1") (compat "29.1.3.4"))
 ;; Homepage: https://github.com/minad/vertico
 
