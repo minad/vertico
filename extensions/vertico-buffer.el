4498a2589c60cc788d6cb3909964c12c8fbad79a
Daniel Mendler <mail@daniel-mendler.de>
2024-06-29 11:21:16 +0200

    Use a better fix

--- extensions/vertico-buffer.el
+++ extensions/vertico-buffer.el
@@ -180,9 +180,10 @@
                   (set-window-parameter win 'no-delete-other-windows old-no-delete)
                   (set-window-dedicated-p win nil)
                   (set-window-buffer win old-buf))
-                 ((window-live-p win)
-                  ;; The window may be a sole window (gh:minad/vertico#496).
-                  (ignore-errors (delete-window win))))
+                 ;; Check `window-parent' since the window may be a sole window
+                 ;; (gh:minad/vertico#496).
+                 ((and (window-live-p win) (window-parent win))
+                  (delete-window win)))
                 (when vertico-buffer-hide-prompt
                   (set-window-vscroll nil 0))))))
     ;; We cannot use a buffer-local minibuffer-exit-hook here.  The hook will
