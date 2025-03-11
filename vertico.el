026a81a9c893b1d73cdbcb12436a0fad3ebdeb5f
Daniel Mendler <mail@daniel-mendler.de>
2025-03-11 17:55:44 +0100

    Version 2.0

--- vertico.el
+++ vertico.el
@@ -5,7 +5,7 @@
 ;; Author: Daniel Mendler <mail@daniel-mendler.de>
 ;; Maintainer: Daniel Mendler <mail@daniel-mendler.de>
 ;; Created: 2021
-;; Version: 1.11
+;; Version: 2.0
 ;; Package-Requires: ((emacs "28.1") (compat "30"))
 ;; URL: https://github.com/minad/vertico
 ;; Keywords: convenience, files, matching, completion
