215b40ee46c192489cd325963765479fe1bf4076
Daniel Mendler <mail@daniel-mendler.de>
2025-05-16 15:42:39 +0200

    Update docstring

--- vertico.el
+++ vertico.el
@@ -464,7 +464,7 @@ The value should lie between 0 and vertico-count/2."
       (setq beg next))))
 
 (defun vertico--debug (&rest _)
-  "Debugger used by `vertico--guard'."
+  "Debugger used by `vertico--protect'."
   (require 'backtrace)
   (declare-function backtrace-to-string "backtrace")
   (declare-function backtrace-get-frames "backtrace")
