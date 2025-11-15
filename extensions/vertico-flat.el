8cff876a15203d24c42e19585fe6dcce8f735bbe
Daniel Mendler <mail@daniel-mendler.de>
2025-11-15 19:26:35 +0100

    Version 2.6

--- extensions/vertico-flat.el
+++ extensions/vertico-flat.el
@@ -5,8 +5,8 @@
 ;; Author: Daniel Mendler <mail@daniel-mendler.de>
 ;; Maintainer: Daniel Mendler <mail@daniel-mendler.de>
 ;; Created: 2021
-;; Version: 2.5
-;; Package-Requires: ((emacs "29.1") (compat "30") (vertico "2.5"))
+;; Version: 2.6
+;; Package-Requires: ((emacs "29.1") (compat "30") (vertico "2.6"))
 ;; URL: https://github.com/minad/vertico
 
 ;; This file is part of GNU Emacs.
